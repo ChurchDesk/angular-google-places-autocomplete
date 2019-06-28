a36617eca3e2b4963a79058adb1cd728afe778cd
Carlos Weckeßer <carlosweckesser@gmail.com>
2019-06-28 10:42:25 +0200

    refactor: Use toLowe() function and build minified version of the autocomplete library

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -293,7 +293,7 @@ angular.module('google.places', [])
                         if (isString(modelValue)) {
                             viewValue = modelValue;
                         } else if (isObject(modelValue)) {
-                            var shouldIncludeName = isString(modelValue.formatted_address) && isString(modelValue.name) && modelValue.formatted_address.toLowerCase().indexOf(modelValue.name.toLowerCase()) < 0;
+                            var shouldIncludeName = isString(modelValue.formatted_address) && isString(modelValue.name) && toLower(modelValue.formatted_address).indexOf(toLower(modelValue.name)) < 0;
                             var name = shouldIncludeName ? modelValue.name + ", " : "";
                             viewValue = name + modelValue.formatted_address;
                         }
